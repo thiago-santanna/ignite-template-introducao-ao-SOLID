c1b35b868748dbf5f51e280f2bb3c561a89a9c8a
thiago-santanna <uzmaki_thiago@outlook.com>
2022-10-07 14:54:57 -0300

    add api-doc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerDocuments from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocuments));
 app.use("/users", usersRoutes);
 
 export { app };
